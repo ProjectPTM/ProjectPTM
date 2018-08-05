033e9a04659906e7e80681a448ae93af9d43200b
marjika <bluegoo5@hotmail.com>
2018-08-05 15:49:03 -0600

    with photo upload functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,22 +2,36 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/toys", function(req, res) {
+    db.Toy.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  // app.put("/api/toys/:id", function(req, res) {
+  //   db.Toy.findAll.update({
+  //     photoPath: req.body.photoPath
+  //   }, {
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   }).then(function(dbBurger) {
+  //     res.json(dbBurger);
+  //   })
+  //     .catch(function(err) {
+  //       res.json(err);
+  //     });
+  // });
+
+  app.post("/api/toys", function(req, res) {
+    db.Toy.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/toys/:id", function(req, res) {
+    db.Toy.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
